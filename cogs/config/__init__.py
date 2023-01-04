aa6eaaee2467fe8f2e26151fc930ec49cdea027b
Pukimaa <pukima@pukima.site>
2023-01-04 14:25:53 +0000

    bump: version 1.3.0 → 1.3.1

--- cogs/config/__init__.py
+++ cogs/config/__init__.py
@@ -3,6 +3,7 @@ from .color import Color
 from .crosspost import Crosspost
 from .language import Language
 from .selfroles import Selfroles
+from .welcome import Welcome
 
 
 async def setup(bot):
@@ -11,3 +12,4 @@ async def setup(bot):
     await bot.add_cog(Crosspost(bot))
     await bot.add_cog(Language(bot))
     await bot.add_cog(Selfroles(bot))
+    await bot.add_cog(Welcome(bot))
