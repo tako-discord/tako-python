83bcb53a9931b53803f9e3a933f3347f4aa15d5b
Pukimaa <pukima@pukima.site>
2023-01-04 15:07:52 +0000

    fix(cogs/config): fix import error

--- cogs/config/__init__.py
+++ cogs/config/__init__.py
@@ -3,7 +3,6 @@ from .color import Color
 from .crosspost import Crosspost
 from .language import Language
 from .selfroles import Selfroles
-from .welcome import Welcome
 
 
 async def setup(bot):
@@ -12,4 +11,3 @@ async def setup(bot):
     await bot.add_cog(Crosspost(bot))
     await bot.add_cog(Language(bot))
     await bot.add_cog(Selfroles(bot))
-    await bot.add_cog(Welcome(bot))
