b1766801ba5a978577d380f67a155c1982eebcf1
Gudo <109687893+Gudorlltti@users.noreply.github.com>
2023-01-07 10:54:54 +0100

    roles

--- cogs/moderation/roles.py
+++ cogs/moderation/roles.py
@@ -27,14 +27,43 @@ class Roles(commands.Cog):
             interaction.reply(i18n.t("moderation.roles_provide_reason", locale=language))
             return
         
-        memberTop = member.top_role
-        interactionTop = interaction.user.top_role
-        roleList = interaction.guild.roles
-        if roleList[memberTop] > roleList[interactionTop]:
-            await member.add_roles(role, reason=f"Role added by {interaction.user}, reson: {reason}")
+        member_top = member.top_role
+        interaction_top = interaction.user.top_role
+        role_list = interaction.guild.roles
+        if roleList[member_top] > roleList[interaction_top]:
+            await member.add_roles(role, reason=f"Role added by {interaction.user}, reason: {reason}")
             await interaction.reply(f"{member.name} has been given the role {role.name}.")
         else:
             await interaction.reply(f"You cant add a role to that user!")
+
+
+    @app_commands.command(description="Removes a role from the provided member.")
+    @app_commands.checks.has_permissions(manage_channels=True)
+    @app_commands.checks.bot_has_permissions(manage_channels=True)
+    @app_commands.describe(member="The member to remove the role from.")
+    @app_commands.describe(role="The role to remove from the member.")
+    @app_commands.describe(reason="Why are you removing the role?")
+    async def removerole(self, interaction: discord.Interaction, member: discord.Member = None, role: discord.Role = None, reason = None, aliases=["remrole"]):
+        language = get_language(self.bot, interaction.guild.id)
+
+        if member is None:
+            interaction.reply(i18n.t("moderation.roles_provide_member", locale=language))
+            return
+        if role is None:
+            interaction.reply(i18n.t("moderation.roles_provide_role", locale=language))
+            return
+        if reason is None:
+            interaction.reply(i18n.t("moderation.roles_provide_reason", locale=language))
+            return
+        
+        member_top = member.top_role
+        interaction_top = interaction.user.top_role
+        role_list = interaction.guild.roles
+        if roleList[member_top] > roleList[interaction_top]:
+            await member.remove_roles(role, reason=f"Role removed by {interaction.user}, reason: {reason}")
+            await interaction.reply(f"{role.name} has been removed from {member.name}.")
+        else:
+            await interaction.reply(f"You can't remove a role from that user!")
 
     @app_commands.command(description="Adds a role to every member")
     @app_commands.checks.has_permissions(manage_channels=True)
@@ -42,5 +71,34 @@ class Roles(commands.Cog):
     @app_commands.describe(role="The role to add to everyone.")
     @app_commands.describe(reason="Why are you adding the role?")
     @app_commands.describe(option="[1] Everyone  [2] Only Human  [3] Only Bots")
-    async def addroletoeveryone(self, interaction: discord.Interaction, role: discord.Role = None, reason = None):
-        pass
+    async def addroletoeveryone(self, interaction: discord.Interaction, role: discord.Role = None, reason = None, option = 1):
+        language = get_language(self.bot, interaction.guild.id)
+        if role is None:
+            interaction.reply(i18n.t("moderation.roles_provide_role", locale=language))
+            return
+        if reason is None:
+            interaction.reply(i18n.t("moderation.roles_provide_reason", locale=language))
+            return
+        
+        all_members = interaction.guild.members(limit=None)
+        counter = 0
+        if option == 1:
+            # @everyone
+            for user in all_members:
+                counter =+ 1
+                await user.add_roles(role, reason=f"Role added by {interaction.user}, reason: {reason}")
+            await interaction.reply(f"{role.name} has been given to {counter} Members."  
+        elif option == 2:
+            # @humans
+            for user in all_members:
+                if not user.bot:
+                    counter =+ 1
+                    await user.add_roles(role, reason=f"Role added by {interaction.user}, reason: {reason}")
+            await interaction.reply(f"{role.name} has been given to {counter} Humans."        
+        elif option == 3:
+            # @bots
+            for user in all_members:
+                if user.bot:
+                    counter =+ 1
+                    await user.add_roles(role, reason=f"Role added by {interaction.user}, reason: {reason}")
+            await interaction.reply(f"{role.name} has been given to {counter} Bots.")
