68372538e9a63f5c4ca2af8c812a07b2c17221d7
Pukimaa <pukima@pukima.site>
2023-01-07 17:51:53 +0000

    feat(economy): remove gambling
    Why? We need to remove gambling in order to be accepted for App Directory. No worries! The code is still there and we will create a new bot with the same database just for gambling.

--- cogs/economy/__init__.py
+++ cogs/economy/__init__.py
@@ -1,11 +1,13 @@
 from .balance import Balance
 from .bank import Bank
-from .gamble import Gamble
 from .give import Give
+# We need to remove gambling in order to comply with the Discord Guidelines for App Directory
+# Our plan is to make a seperate bot for gambling, that will have shared data with this bot
+# from .gamble import Gamble
 
 
 async def setup(bot):
     await bot.add_cog(Balance(bot))
     await bot.add_cog(Bank(bot))
-    await bot.add_cog(Gamble(bot))
     await bot.add_cog(Give(bot))
+#   await bot.add_cog(Gamble(bot))
