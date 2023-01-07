7b236fccb95eac17f04bbac2738bcd42ea1f2e5a
Pukimaa <pukima@pukima.site>
2023-01-07 22:20:17 +0000

    feat(moderation): add slowmode command
    This also allows users to set the slowmode below 5 seconds (like 3s etc.)

--- cogs/moderation/__init__.py
+++ cogs/moderation/__init__.py
@@ -2,6 +2,7 @@ from .anti_phishing import AntiPhishing
 from .ban_game import BanGame
 from .channel_locking import ChannelLocking
 from .clear import Clear
+from .slowmode import Slowmode
 
 
 async def setup(bot):
@@ -9,3 +10,4 @@ async def setup(bot):
     await bot.add_cog(BanGame(bot))
     await bot.add_cog(ChannelLocking(bot))
     await bot.add_cog(Clear(bot))
+    await bot.add_cog(Slowmode(bot))
