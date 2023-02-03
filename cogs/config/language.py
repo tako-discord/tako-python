f3b884899e38347319373f3260b07936d82d1d00
Pukimaa <pukima@pukima.site>
2023-02-03 12:26:14 +0000

    feat(languages): add Japanese (3%) and Tagalog (5%)

--- cogs/config/language.py
+++ cogs/config/language.py
@@ -14,26 +14,29 @@ class Language(commands.Cog):
     )
     @app_commands.checks.has_permissions(manage_guild=True)
     @app_commands.describe(language="The language to set the bot to")
+    @app_commands.guild_only()
     @app_commands.choices(
         language=[
-            Choice(name="English", value="en"),
-            Choice(name="Deutsch", value="de"),
-            Choice(name="Español", value="es"),
-            Choice(name="Français", value="fr"),
-            Choice(name="עִברִית", value="he"),
-            Choice(name="Hrvatski", value="hr"),
-            Choice(name="Indonesia", value="id"),
-            Choice(name="Nederlands", value="nl"),
-            Choice(name="Polski", value="pl"),
-            Choice(name="Português (brasileiro)", value="pt"),
-            Choice(name="Svenska", value="sv"),
+            Choice(name="English (Default)", value="en"),
+            Choice(name="Deutsch (German)", value="de"),
+            Choice(name="Español (Spanish)", value="es"),
+            Choice(name="Français (French)", value="fr"),
+            Choice(name="Hrvatski (Croatian)", value="hr"),
+            Choice(name="Indonesia (Indonesian)", value="id"),
+            Choice(name="Mga Tagalog (Tagalog)", value="tl"),
+            Choice(name="Nederlands (Dutch)", value="nl"),
+            Choice(name="Polski (Polish)", value="pl"),
+            Choice(name="Português, brasileiro (Portuguese, Brazilian)", value="pt"),
+            Choice(name="Svenska (Swedish)", value="sv"),
+            Choice(name="עִברִית (Hebrew)", value="he"),
+            Choice(name="日本 (Japanese)", value="ja"),
         ]
     )
     async def set_language(self, interaction: discord.Interaction, language: str):
         async with self.bot.db_pool.acquire() as conn:
             await conn.execute(
                 "INSERT INTO guilds(guild_id, language) VALUES($1, $2) ON CONFLICT(guild_id) DO UPDATE SET guild_id = $1, language = $2",
-                interaction.guild.id,
+                interaction.guild_id,
                 language,
             )
             data = await conn.fetch("SELECT * FROM guilds")
