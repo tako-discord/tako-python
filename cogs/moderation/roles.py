d1bd5bcf5637b88c66a4adad005ee69e11c23841
Gudo <109687893+Gudorlltti@users.noreply.github.com>
2023-02-09 19:29:22 +0100

    Update roles.py

--- cogs/moderation/roles.py
+++ cogs/moderation/roles.py
@@ -16,28 +16,19 @@ class Roles(commands.GroupCog, name="role"):
     @app_commands.describe(role="The role to add to the member.")
     @app_commands.describe(reason="Why are you adding the role?")
     @app_commands.guild_only()
-    async def add(
-        self,
-        interaction: discord.Interaction,
-        role: discord.Role,
-        member: discord.Member = None,
-        reason: str = None,
-    ):
+    async def add(self, interaction: discord.Interaction, role: discord.Role, member: discord.Member = None, reason: str = None):
         # language = get_language(self.bot, interaction.guild.id)
 
-        if not member:
-            member = interaction.user
+        if not member:member = interaction.user
 
-        if role >= interaction.user.top_role:
-            return await interaction.response.send_message(
-                f"{role.name} is either above you or me in the Role list."
-            )
-        await member.add_roles(
-            role, reason=f"Role added by {interaction.user}, reason: {reason}"
-        )
-        await interaction.response.send_message(
-            f"{member.name} has been given the role {role.name}."
-        )
+        if role.position >= interaction.user.top_role.position:
+            embed = discord.Embed(colour=discord.Colour(error_color), description=f"{role.mention} is either above you or me in the Role list.")
+            return await interaction.response.send_message(embed=embed, ephemeral=True)
+        
+        await member.add_roles(role, reason=f"Role added by {interaction.user}, reason: {reason}")
+        
+        embed = discord.Embed(colour=discord.Colour(success_color), description=f"The role {role.mention} was successfully **given** to {member.mention}!")
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
     @app_commands.command(description="Removes a role from the provided member.")
     @app_commands.checks.has_permissions(manage_roles=True)
@@ -45,28 +36,19 @@ class Roles(commands.GroupCog, name="role"):
     @app_commands.describe(member="The member to remove the role from.")
     @app_commands.describe(role="The role to remove from the member.")
     @app_commands.describe(reason="Why are you removing the role?")
-    async def remove(
-        self,
-        interaction: discord.Interaction,
-        role: discord.Role,
-        member: discord.Member = None,
-        reason: str = None,
-    ):
-        language = get_language(self.bot, interaction.guild.id)
+    async def remove(self, interaction: discord.Interaction, role: discord.Role, member: discord.Member = None, reason: str = None):
+        # language = get_language(self.bot, interaction.guild.id)
 
-        if not member:
-            member = interaction.user
+        if not member: member = interaction.user
 
-        if role >= interaction.user.role:
-            return await interaction.response.send_message(
-                f"{role.name} is either above you or me in the Role list."
-            )
-        await member.remove_roles(
-            role, reason=f"Role added by {interaction.user}, reason: {reason}"
-        )
-        await interaction.response.send_message(
-            f"{member.name} has been given the role {role.name}."
-        )
+        if role.position >= interaction.user.top_role.position:
+            embed = discord.Embed(colour=discord.Colour(error_color), description=f"{role.mention} is either above you or me in the Role list.")
+            return await interaction.response.send_message(embed=embed, ephemeral=True)
+        
+        await member.remove_roles(role, reason=f"Role added by {interaction.user}, reason: {reason}")
+        
+        embed = discord.Embed(colour=discord.Colour(success_color), description=f"The role {role.mention} was successfully **removed** from {member.mention}!")
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
     @app_commands.command(description="Adds a role to every member")
     @app_commands.checks.has_permissions(manage_roles=True)
