a30ce528654a7484032dd45f74baf89b85497581
Gudo <109687893+Gudorlltti@users.noreply.github.com>
2023-02-10 19:58:19 +0100

    Update roles.py

--- cogs/moderation/roles.py
+++ cogs/moderation/roles.py
@@ -5,9 +5,9 @@ from discord.ext import commands
 from discord import app_commands
 
 
-error_color = 0xD0021B
-success_color = 0x7ED321
-
+error_color = 0xd0021b
+success_color = 0x7ed321
+loading_color = 0xf5a623
 
 class Roles(commands.GroupCog, name="role"):
     def __init__(self, bot: commands.bot):
@@ -20,40 +20,20 @@ class Roles(commands.GroupCog, name="role"):
     @app_commands.describe(role="The role to add to the member.")
     @app_commands.describe(reason="Why are you adding the role?")
     @app_commands.guild_only()
-    async def add(
-        self,
-        interaction: discord.Interaction,
-        role: discord.Role,
-        member: discord.Member = None,
-        reason: str = None,
-    ):
-        # language = get_language(self.bot, interaction.guild.id)
-
-        if not member:
-            member = interaction.user
-
+    async def add(self, interaction: discord.Interaction, role: discord.Role, member: discord.Member = None, reason: str = None):
+        if not member: member = interaction.user       
         if role.position >= interaction.user.top_role.position:
-            embed = discord.Embed(
-                colour=discord.Colour(error_color),
-                description=f"{role.mention} is either above you or me in the Role list.",
-            )
+            embed = discord.Embed(colour=discord.Colour(error_color), description=f"{role.mention} is either above you or me in the Role list.")
             return await interaction.response.send_message(embed=embed, ephemeral=True)
         if role in member.roles:
-            embed = discord.Embed(
-                colour=discord.Colour(error_color),
-                description=f"The user already has the {role.mention}!",
-            )
+            embed = discord.Embed(colour=discord.Colour(error_color), description=f"The user already has the {role.mention}!")
             return await interaction.response.send_message(embed=embed, ephemeral=True)
-
-        await member.add_roles(
-            role, reason=f"Role added by {interaction.user}, reason: {reason}"
-        )
-
-        embed = discord.Embed(
-            colour=discord.Colour(success_color),
-            description=f"The role {role.mention} was successfully **given** to {member.mention}!",
-        )
+        
+        await member.add_roles(role, reason=f"Role added by {interaction.user}, reason: {reason}")
+        
+        embed = discord.Embed(colour=discord.Colour(success_color), description=f"The role {role.mention} was successfully **given** to {member.mention}!")
         await interaction.response.send_message(embed=embed, ephemeral=True)
+
 
     @app_commands.command(description="Removes a role from the provided member.")
     @app_commands.checks.has_permissions(manage_roles=True)
@@ -61,40 +41,20 @@ class Roles(commands.GroupCog, name="role"):
     @app_commands.describe(member="The member to remove the role from.")
     @app_commands.describe(role="The role to remove from the member.")
     @app_commands.describe(reason="Why are you removing the role?")
-    async def remove(
-        self,
-        interaction: discord.Interaction,
-        role: discord.Role,
-        member: discord.Member = None,
-        reason: str = None,
-    ):
-        # language = get_language(self.bot, interaction.guild.id)
-
-        if not member:
-            member = interaction.user
-
+    async def remove(self, interaction: discord.Interaction, role: discord.Role, member: discord.Member = None, reason: str = None):
+        if not member: member = interaction.user
         if role.position >= interaction.user.top_role.position:
-            embed = discord.Embed(
-                colour=discord.Colour(error_color),
-                description=f"{role.mention} is either above you or me in the Role list.",
-            )
+            embed = discord.Embed(colour=discord.Colour(error_color), description=f"{role.mention} is either above you or me in the Role list.")
             return await interaction.response.send_message(embed=embed, ephemeral=True)
         if not role in member.roles:
-            embed = discord.Embed(
-                colour=discord.Colour(error_color),
-                description=f"The user does not have the role {role.mention}!",
-            )
+            embed = discord.Embed(colour=discord.Colour(error_color), description=f"The user does not have the role {role.mention}!")
             return await interaction.response.send_message(embed=embed, ephemeral=True)
-
-        await member.remove_roles(
-            role, reason=f"Role added by {interaction.user}, reason: {reason}"
-        )
-
-        embed = discord.Embed(
-            colour=discord.Colour(success_color),
-            description=f"The role {role.mention} was successfully **removed** from {member.mention}!",
-        )
+        
+        await member.remove_roles(role, reason=f"Role added by {interaction.user}, reason: {reason}")
+        
+        embed = discord.Embed(colour=discord.Colour(success_color), description=f"The role {role.mention} was successfully **removed** from {member.mention}!")
         await interaction.response.send_message(embed=embed, ephemeral=True)
+
 
     @app_commands.command(description="Adds a role to every member")
     @app_commands.checks.has_permissions(manage_roles=True)
@@ -102,108 +62,55 @@ class Roles(commands.GroupCog, name="role"):
     @app_commands.describe(role="The role to add to everyone selected")
     @app_commands.describe(reason="Why are you adding the role")
     @app_commands.describe(option="Select to who the role should be added")
-    @app_commands.choices(
-        option=[
-            app_commands.Choice(name="Everyone", value=1),
-            app_commands.Choice(name="Only Humans", value=2),
-            app_commands.Choice(name="Only self.bots", value=3),
-        ]
-    )
-    async def add_all(
-        self,
-        interaction: discord.Interaction,
-        role: discord.Role,
-        option: int = 1,
-        reason: str = None,
-    ):
-        # language = get_language(self.bot, interaction.guild.id)
+    @app_commands.choices(option=[app_commands.Choice(name="Everyone", value=1), app_commands.Choice(name="Only Humans", value=2), app_commands.Choice(name="Only self.bots", value=3),])
+    async def add_all(self, interaction: discord.Interaction, role: discord.Role, option: int = 1, reason: str = None):
+        if role.position > interaction.user.top_role.position: return await interaction.response.send_message(f"You can't add this role to the members!")   
 
-        if role.position > interaction.user.top_role.position:
-            return await interaction.response.send_message(
-                f"You can't add this role to the members!"
-            )
-
-        all_members = interaction.guild.members
+        member_list = interaction.guild.members
         counter = 0
-        have = 0
-        blocked = []  # above the user
-        failed = []  # above the bot
-
-        # TODO: Add failed and blocked to 2nd and 3rd option
-        if option == 1:
-            # @everyone
-            for user in all_members:
-                if not role in user.roles:
-                    user_top = user.top_role
-
-                    member = interaction.guild.get_member(1061216305040064532)
-                    self.bot_top = member.top_role
-
-                    interaction_top = interaction.user.top_role
-                    role_list = interaction.guild.roles
-                    if user.top_role >= self.bot_top:
-                        failed.append(user.name)
+        
+        bot_top = interaction.guild.get_member(self.bot.user.id).top_role.position
+        interaction_top = interaction.user.top_role.position
+        
+        embed = discord.Embed(colour=discord.Colour(loading_color), description=f"<a:loading:1071777312128905267> **Adding** the role {role.mention} to {len(member_list)} members...\nETA: soon:tm:")
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        if option == 1: #everyone
+            for user in member_list:
+                user_top = user.top_role.position
+                if not role in user.roles:                   
+                    if user_top > bot_top:
                         continue
-                    if user.top_role >= interaction.user.top_role:
-                        blocked.append(user.name)
+                    if user_top > interaction_top:
                         continue
                     counter += 1
-                    await user.add_roles(
-                        role,
-                        reason=f"Role added by {interaction.user}, reason: {reason}",
-                    )
-                else:
-                    have += 1
+                    await user.add_roles(role, reason=f"Role added by {interaction.user}, reason: {reason}")
+            
+            if counter == 0: embed = discord.Embed(colour=discord.Colour(error_color), description=f"All members are already having the role {role.mention}.")
+            else: embed = discord.Embed(colour=discord.Colour(success_color), description=f"The role {role.mention} was successfully **given** to {counter} members!")
+            return await interaction.edit_original_response(embed=embed)
 
-            lblocked = len(blocked)
-            lfailed = len(failed)
-            if lblocked >= 1:
-                embed = discord.Embed(
-                    colour=discord.Colour(success_color),
-                    description=f"The role {role.mention} was successfully **given** to {counter} members!\n(Blocked: {lblocked})",
-                )
-            if lfailed >= 1:
-                embed = discord.Embed(
-                    colour=discord.Colour(success_color),
-                    description=f"The role {role.mention} was successfully **given** to {counter} members!\n(Failed: {lfailed})",
-                )
-            if lfailed >= 1 and lblocked >= 1:
-                embed = discord.Embed(
-                    colour=discord.Colour(success_color),
-                    description=f"The role {role.mention} was successfully **given** to {counter} members!\n(Blocked: {lblocked}, Failed: {lfailed})",
-                )
-            else:
-                embed = discord.Embed(
-                    colour=discord.Colour(success_color),
-                    description=f"The role {role.mention} was successfully **given** to {counter} members!",
-                )
-            return await interaction.response.send_message(embed=embed, ephemeral=True)
-
-        if option == 2:
-            # @humans
-            for user in all_members:
+        if option == 2: # humans
+            for user in member_list:
                 if not user.self.bot:
-                    counter = +1
-                    await user.add_roles(
-                        role,
-                        reason=f"Role added by {interaction.user}, reason: {reason}",
-                    )
-            return await interaction.response.send_message(
-                f"{role.name} has been given to {counter} Humans."
-            )
+                    user_top = user.top_role.position
+                    if not role in user.roles:
+                        counter += 1
+                        await user.add_roles(role, reason=f"Role added by {interaction.user}, reason: {reason}")
+            if counter == 0: embed = discord.Embed(colour=discord.Colour(error_color), description=f"All humans are already having the role {role.mention}.")
+            else: embed = discord.Embed(colour=discord.Colour(success_color), description=f"The role {role.mention} was successfully **given** to {counter} humans!")
+            return await interaction.edit_original_response(embed=embed)
 
-        if option == 3:
-            # @self.bots
-            for user in all_members:
-                if user.self.bot:
-                    counter = +1
-                    await user.add_roles(
-                        role,
-                        reason=f"Role added by {interaction.user}, reason: {reason}",
-                    )
-            return await interaction.response.send_message(
-                f"{role.name} has been given to {counter} Bots."
-            )
+        if option == 3: # bots
+            for user in member_list:      
+                if user.bot:
+                    user_top = user.top_role.position
+                    if not role in user.roles:    
+                        counter += 1
+                        await user.add_roles(role, reason=f"Role added by {interaction.user}, reason: {reason}")
+            if counter == 0: embed = discord.Embed(colour=discord.Colour(error_color), description=f"All bots are already having the role {role.mention}.")
+            else: embed = discord.Embed(colour=discord.Colour(success_color), description=f"The role {role.mention} was successfully **given** to {counter} bots!")
+            return await interaction.edit_original_response(embed=embed)
+
 
     @app_commands.command(description="Removes a role from every member")
     @app_commands.checks.has_permissions(manage_roles=True)
@@ -211,48 +118,38 @@ class Roles(commands.GroupCog, name="role"):
     @app_commands.describe(role="The role to remove from everyone.")
     @app_commands.describe(reason="Why are you removing the role?")
     @app_commands.describe(option="[1] Everyone  [2] Only Humans  [3] Only Bots")
-    async def remove_all(
-        self,
-        interaction: discord.Interaction,
-        role: discord.Role,
-        reason: str = None,
-        option: int = 1,
-    ):
-        all_members = interaction.guild.members
+    async def remove_all(self, interaction: discord.Interaction, role: discord.Role, reason: str = None, option: int = 1):
+        member_list = interaction.guild.members
         counter = 0
-        if option == 1:
-            # @everyone
-            for user in all_members:
+        
+        embed = discord.Embed(colour=discord.Colour(loading_color), description=f"<a:loading:1071777312128905267> **Removing** the role {role.mention} from {len(member_list)} members...\nETA: soon:tm:")
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        if option == 1: # everyone
+            for user in member_list:
                 if role in user.roles:
                     counter += 1
-                    await user.remove_roles(
-                        role,
-                        reason=f"Role removed by {interaction.user}, reason: {reason}",
-                    )
-            await interaction.response.send_message(
-                f"{role.name} has been removed from {counter} Members."
-            )
-        elif option == 2:
-            # @humans
-            for user in all_members:
-                if not user.self.bot:
-                    counter = +1
-                    await user.remove_roles(
-                        role,
-                        reason=f"Role removed by {interaction.user}, reason: {reason}",
-                    )
-            await interaction.response.send_message(
-                f"{role.name} has been removed from {counter} Humans."
-            )
-        elif option == 3:
-            # @self.bots
-            for user in all_members:
-                if user.self.bot:
-                    counter = +1
-                    await user.remove_roles(
-                        role,
-                        reason=f"Role removed by {interaction.user}, reason: {reason}",
-                    )
-            await interaction.response.send_message(
-                f"{role.name} has been removed from {counter} self.bots."
-            )
+                    await user.remove_roles(role, reason=f"Role removed by {interaction.user}, reason: {reason}")
+            
+            if counter == 0: embed = discord.Embed(colour=discord.Colour(error_color), description=f"No one has the role {role.mention}.")
+            else: embed = discord.Embed(colour=discord.Colour(success_color), description=f"The role {role.mention} was successfully **removed** from {counter} members!")
+            return await interaction.edit_original_response(embed=embed)
+        
+        elif option == 2: # humans
+            for user in member_list:
+                if not user.bot:
+                    counter += 1
+                    await user.remove_roles(role, reason=f"Role removed by {interaction.user}, reason: {reason}")
+            
+            if counter == 0: embed = discord.Embed(colour=discord.Colour(error_color), description=f"No one has the role {role.mention}.")        
+            else: embed = discord.Embed(colour=discord.Colour(success_color), description=f"The role {role.mention} was successfully **removed** from {counter} humans!")
+            return await interaction.edit_original_response(embed=embed)   
+             
+        elif option == 3: #bots
+            for user in member_list:
+                if user.bot:
+                    counter += 1
+                    await user.remove_roles(role, reason=f"Role removed by {interaction.user}, reason: {reason}")
+            
+            if counter == 0: embed = discord.Embed(colour=discord.Colour(error_color), description=f"No one has the role {role.mention}.")        
+            else: embed = discord.Embed(colour=discord.Colour(success_color), description=f"The role {role.mention} was successfully **removed** from {counter} bots!")
+            return await interaction.edit_original_response(embed=embed)
