7b48748e4ce6f208daacb81de152d5378ec05e50
Pukimaa <pukima@pukima.site>
2023-02-13 13:38:26 +0000

    refactor(uwuify): add descriptions for fields and use percentage from 0-100 instead of 0-1

--- cogs/misc/uwu.py
+++ cogs/misc/uwu.py
@@ -10,15 +10,22 @@ class UwU(commands.Cog):
     def __init__(self, bot: TakoBot):
         self.bot = bot
 
-    @app_commands.command(description="uwuify text uwu")
+    @app_commands.command(description="uwuify t~text uwu")
+    @app_commands.describe(
+        message="Your message",
+        stutter_chance="Chance of s~s~stuttering (in %, default: 10%)",
+        face_chance="Chance of adding a face ^w^ (in %, default: 0%)",
+        action_chance="Chance of adding an action *blushes* (in %, default: 5%)",
+        exclamation_chance="Chance of adding an exclamation!1!! (in %, default: 25%)",
+    )
     async def uwuify(
         self,
         interaction: discord.Interaction,
         message: str,
-        stutter_chance: app_commands.Range[float, 0, 1] = 0.1,
-        face_chance: app_commands.Range[float, 0, 1] = 0,
-        action_chance: app_commands.Range[float, 0, 1] = 0,
-        exclamation_chance: app_commands.Range[float, 0, 1] = 0.5,
+        stutter_chance: app_commands.Range[int, 0, 100] = 10,
+        face_chance: app_commands.Range[int, 0, 100] = 0,
+        action_chance: app_commands.Range[int, 0, 100] = 5,
+        exclamation_chance: app_commands.Range[int, 0, 100] = 25,
     ):
         await interaction.response.defer()
         if interaction.guild_id:
@@ -28,10 +35,10 @@ class UwU(commands.Cog):
                 message = translated
         uwu = uwuipy(
             round(interaction.created_at.timestamp()),
-            stutter_chance=stutter_chance,
-            face_chance=face_chance,
-            action_chance=action_chance,
-            exclamation_chance=exclamation_chance,
+            stutter_chance=stutter_chance/100,
+            face_chance=face_chance/100,
+            action_chance=action_chance/100,
+            exclamation_chance=exclamation_chance/100,
         )
-        text = uwu.uwuify(message)
+        text = uwu.uwuify(message).replace("-", "~")
         await interaction.edit_original_response(content=text)
