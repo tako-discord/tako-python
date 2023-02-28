813f99cc819045200fc24bb51e7da69cccf88309
Pukimaa <pukima@pukima.site>
2023-02-28 14:27:52 +0000

    fix(affirmation): fix affirmations not responding
    I also removed the button and added an option to make it ephemeral or not

--- cogs/misc/affirmations.py
+++ cogs/misc/affirmations.py
@@ -3,7 +3,6 @@ import aiohttp
 from TakoBot import TakoBot
 from discord import app_commands
 from discord.ext import commands
-from persistent_views.affirmation_buttons import AffirmationButtons
 
 
 class Affirmations(commands.Cog):
@@ -11,10 +10,14 @@ class Affirmations(commands.Cog):
         self.bot = bot
 
     @app_commands.command(description="Feel bad? Get some affirmations!")
-    async def affirmation(self, interaction: discord.Interaction):
+    @app_commands.describe(
+        ephemeral="Whether the response should be ephemeral (default: false)",
+    )
+    async def affirmation(self, interaction: discord.Interaction, ephemeral: bool = False):
+        await interaction.response.defer(ephemeral=ephemeral)
         async with aiohttp.ClientSession() as session:
             async with session.get("https://affirmations.dev/") as r:
                 data = await r.json()
-                await interaction.response.send_message(
-                    data["affirmation"], view=AffirmationButtons(), ephemeral=True
+                await interaction.followup.send(
+                    data["affirmation"]
                 )
