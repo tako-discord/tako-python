9835be1ec499d69518bbce3aaef2011d77dec4c1
Pukimaa <pukima@pukima.site>
2023-03-15 15:52:06 +0000

    refactor(anti-phishing): temporarily remove the AntiPhishing cog

--- cogs/moderation/__init__.py
+++ cogs/moderation/__init__.py
@@ -1,4 +1,4 @@
-from .anti_phishing import AntiPhishing
+# from .anti_phishing import AntiPhishing
 # from .ban_game import BanGame
 from .channel_locking import ChannelLocking
 from .clear import Clear
@@ -7,8 +7,8 @@ from .warn import Warn
 
 
 async def setup(bot):
-    await bot.add_cog(AntiPhishing(bot))
-    #   await bot.add_cog(BanGame(bot))
+    # await bot.add_cog(AntiPhishing(bot))
+    # await bot.add_cog(BanGame(bot))
     await bot.add_cog(ChannelLocking(bot))
     await bot.add_cog(Clear(bot))
     await bot.add_cog(Slowmode(bot))
