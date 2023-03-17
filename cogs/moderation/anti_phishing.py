5a17d9275210ffe30f0eeb6f8e82e0857f3e424f
Pukimaa <pukima@pukima.site>
2023-03-17 12:20:41 +0000

    fix(anti-phishing): use regex now...finally discord.gift won't be blocked lmao

--- cogs/moderation/anti_phishing.py
+++ cogs/moderation/anti_phishing.py
@@ -1,27 +1,35 @@
-import i18n
+from re import findall
+
 import discord
-from main import TakoBot
 from discord.ext import commands
+
+import i18n
+from main import TakoBot
 from utils import get_language
 
 
-# TODO: Add actual working regex instead of just checking whether it contains something from the list
-# TODO: See https://github.com/nikolaischunk/stop-discord-phishing for reference
 class AntiPhishing(commands.Cog):
     def __init__(self, bot: TakoBot) -> None:
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
+        url_extract_pattern = "[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)"
+
+        urls = findall(
+            url_extract_pattern,
+            message.content.lower(),
+        )
         for domain in self.bot.sussy_domains:
-            if message.content.lower().__contains__(domain):
-                await message.delete()
-                await message.channel.send(
-                    i18n.t(
-                        "moderation.malicious_link",
-                        user=message.author.mention,
-                        locale=get_language(
-                            self.bot, message.guild.id if message.guild else None
-                        ),
+            for url in urls:
+                if url == domain:
+                    await message.delete()
+                    await message.channel.send(
+                        i18n.t(
+                            "moderation.malicious_link",
+                            user=message.author.mention,
+                            locale=get_language(
+                                self.bot, message.guild.id if message.guild else None
+                            ),
+                        )
                     )
-                )
